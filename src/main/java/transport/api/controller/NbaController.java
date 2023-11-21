e6db8432a0b30fea10082f7d174d2f375f8f3713
Mickael158 <razafindrakotomickael77@gmail.com>
2023-11-21 17:15:11 +0300

    2

--- src/main/java/transport/api/controller/NbaController.java
+++ src/main/java/transport/api/controller/NbaController.java
@@ -1,24 +1,24 @@
 package transport.api.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import transport.api.model.Affichage;
 import transport.api.model.ConnexionBDD;
 import transport.api.model.Equipe;
-import transport.api.model.Vehicule;
-import transport.api.service.VehiculeService;
 
 import java.sql.Connection;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/nba")
+@CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
 public class NbaController {
 
+
+
+    @CrossOrigin(origins = "*", methods = {RequestMethod.GET})
     @GetMapping
     public ResponseEntity<List<Affichage>> getAllAffichage() throws SQLException, ClassNotFoundException {
         Affichage affichage = new Affichage();
@@ -28,6 +28,8 @@ public class NbaController {
         connection.close();
         return new ResponseEntity<>(affichages, HttpStatus.OK);
     }
+
+    @CrossOrigin(origins = "*", methods = {RequestMethod.GET})
     @GetMapping("ListeEquipe")
     public ResponseEntity<List<Equipe>> getAllEquipe() throws SQLException, ClassNotFoundException {
         Equipe equipe = new Equipe();
@@ -37,6 +39,8 @@ public class NbaController {
         connection.close();
         return new ResponseEntity<>(equipes, HttpStatus.OK);
     }
+
+    @CrossOrigin(origins = "*", methods = {RequestMethod.GET})
     @GetMapping("StateByEquipe/{idEquipe}")
     public ResponseEntity<List<Affichage>> getAllAffichageByEquipe(@PathVariable int idEquipe) throws SQLException, ClassNotFoundException {
         Affichage affichage = new Affichage();
--- src/main/java/transport/api/model/ConnexionBDD.java
+++ src/main/java/transport/api/model/ConnexionBDD.java
@@ -5,13 +5,13 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnexionBDD {
-    private static final String DATABASE_URL = "jdbc:postgresql://localhost:5432/nba";
+    private static final String DATABASE_URL = "postgresql://postgres:bbcbCf1aeBg3CAaegaE5ga3C-A5bAbeg@viaduct.proxy.rlwy.net:58154/railway";
     private static final String USERNAME = "postgres";
-    private static final String PASSWORD = "12345678";
+    private static final String PASSWORD = "bbcbCf1aeBg3CAaegaE5ga3C-A5bAbeg";
 
     // Méthode pour établir une connexion à la base de données PostgreSQL
     public Connection connect() throws ClassNotFoundException, SQLException {
-        Class.forName("org.postgresql.Driver");
+        //Class.forName("org.postgresql.Driver");
         return DriverManager.getConnection(DATABASE_URL, USERNAME, PASSWORD);
     }
 }
